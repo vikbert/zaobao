fe3b91fc5b221b7c0ab6bd9c5ecba8095d667589
Xun Zhou <xun.zhou@lidl.com>
2019-05-16 17:17:00 +0200

    update 16

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 set -e
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
 yarn run docs:build
 cd docs/.vuepress/dist
 
 git init
 git add -A
+
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
 git commit -m "deploy: github gh-pages $DATE"
 
 git push -f https://github.com/vikbert/zaobao.git master:gh-pages
