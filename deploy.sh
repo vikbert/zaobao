1b61cf7e223605c784929f3fb2089996e15f3ab5
Xun Zhou <xun.zhou@lidl.com>
2019-05-21 11:17:54 +0200

    new article on 2019-05-21 11:17:54

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 set -e
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+git add -A
+git commit -m "new article on $DATE"
 
 ### this will build the md fiels into html static pages, then publish htmls to gh-pages branch
 yarn run docs:build
@@ -7,10 +11,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
 git commit -m "deploy: github gh-pages $DATE"
-
 git push -f https://github.com/vikbert/zaobao.git master:gh-pages
 
 cd -
